edf1332ae698515caf180d6462a70e0a68e521a0
DiegoJSan <alum.diegojesus.sanchez.delcorral@ieslosremedios.org>
2021-02-12 10:34:27 +0100

    Crear clase cartón y numeros aleatórios

--- src/main/java/diegosanchez/bingo/App.java
+++ src/main/java/diegosanchez/bingo/App.java
@@ -3,7 +3,6 @@ package diegosanchez.bingo;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
--- src/main/java/diegosanchez/bingo/Carton.java
+++ src/main/java/diegosanchez/bingo/Carton.java
@@ -4,11 +4,36 @@ import java.util.Random;
 
 public class Carton {
     
+    //Variables
+    int [][] nums;
+    
     public void generar (){ //void = retorno , generar = nombre de la clase. Se declara el método
-        int[][] num = new int[9][3];
+       
+        nums = new int[9][3];
+        
+        for(int y=0; y<3; y++){
+            for(int x=0; x<9; x++){
+                nums[x][y] = getNumAleatorio(1,90);
+            }
+        }
+        this.mostrarPorConsola();       
+    }
+    
+    public void mostrarPorConsola(){
+        for(int y=0; y<3; y++){
+            for(int x=0; x<9; x++){
+                //System.out.println("x=" + x + "y=" + y);
+                System.out.print(nums [x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public int getNumAleatorio(int min, int max){   // get= dame , generar
         Random random = new Random ();
-        int num = random.nextInt(90)+1; 
-        System.out.println(num);
+        // nim = 1, max = 6
+        int num = random.nextInt(max-min+1) + min;    // 3 = cantidad de numeros  5 = el numero maximo
+        return num;
     }
     
 }
